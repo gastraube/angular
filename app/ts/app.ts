308f520c76ae612b9cfeacd99d7f4263d4396446
Gabriel Straube <ga.straube@gmail.com>
2020-08-05 19:20:11 -0300

    add api import

--- app/ts/app.ts
+++ app/ts/app.ts
@@ -1,8 +1,5 @@
-
-
-import { ajaxTransport } from "jquery";
-import { NegociacaoController } from "./controllers/NegociacaoController";
+import { NegociacaoController } from './controllers/NegociacaoController';
 
 const controller = new NegociacaoController();
 $('.form').submit(controller.adiciona.bind(controller));
-
+$('#botao-importa').click(controller.importarDados.bind(controller));
